30ee8093d015dd92a7f943986b9c4246465703c6
Mura_4 <murilo200522@gmail.com>
2024-05-15 09:08:40 -0300

    O Ninja das Frutas GOTY
    O Ninja das Frutas GOTY Edition

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Image fadeImage;
+
+    private Blade blade;
+    private Spawner spawner;
+
+    private int score;
+    private int level;
+
+    private void Awake()
+    {
+        blade = FindObjectOfType<Blade>();
+        spawner = FindObjectOfType<Spawner>();
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+
+        Time.timeScale = 1f;
+
+        blade.enabled = true;
+        spawner.enabled = true;
+
+        score = 0;
+        scoreText.text = score.ToString();
+
+        ClearScene();
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+
+        foreach (Fruit fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+
+        foreach (Bomb bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score+=amount;
+        scoreText.text = score.ToString();
+
+        if (score >= 20)
+        {
+            StartCoroutine(ExplodeSequence());
+            LoadScene();
+        }
+    }
+
+    public void Explode()
+    {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        LoadScene();
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
